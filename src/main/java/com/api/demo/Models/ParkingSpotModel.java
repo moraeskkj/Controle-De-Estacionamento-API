dee28ede198c92779a5e9eddc308060fff676b29
moraes <mooraesz123@gmail.com>
2023-04-03 13:18:54 -0300

    Novo projeto usando SpringBoot

---
+++ src/main/java/com/api/demo/Models/ParkingSpotModel.java
@@ -0,0 +1,12 @@
+package com.api.demo.Models;
+
+import jakarta.persistence.Entity;
+
+
+/** é difinido usando o spring boot como Entity,já que estamos construindo uma api para controle de vagas de estacionamento,um parkingSpot(vaga) é uma entidade/recurso principal
+ * 
+ * */
+@Entity
+public class ParkingSpotModel {
+    
+}
